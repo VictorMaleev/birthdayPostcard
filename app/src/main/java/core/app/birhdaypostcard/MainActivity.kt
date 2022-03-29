db93e9ca68a5d7840b21c4bbe60f4e05b49fcb8a
VictorMaleev <viktor100800@gmail.com>
2022-03-29 16:22:23 +0300

    adding files

---
+++ app/src/main/java/core/app/birhdaypostcard/MainActivity.kt
@@ -0,0 +1,11 @@
+package core.app.birhdaypostcard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
